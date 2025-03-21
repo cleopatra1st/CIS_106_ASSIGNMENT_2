292d7d49a5fbcaeffab9db6199c61f91d1b10fc0
Cleopatra1st <bronson-johna@ncmich.edu>
2025-03-20 21:14:21 -0400

    InitialCommit

--- services/ProgressReportGenerator.cs
+++ services/ProgressReportGenerator.cs
@@ -9,6 +9,22 @@ namespace CIS_106_ASSIGNMENT_2.services
         public static void GenerateProgressReport(List<Character> characters, List<Level> levels)
         {
             // TODO: Write your code here to generate the progress report.
+           foreach(var character in characters){
+                Console.WriteLine($"Progress Report for {character.Name}:");
+                foreach(var level in levels){
+                    Console.WriteLine($"--{level.Name} Power Ups Collected:");
+                    foreach(var levelPowerUp in level.PowerUps){
+                        bool isCollected = character.PowerUps.Any(characterPowerUp => characterPowerUp.Name == levelPowerUp.Name);
+                        Console.WriteLine($"----{levelPowerUp.Name}:[{(isCollected ? "X" : " ")}]");
+                    
+                    
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine();
+           }
         }
     }
 
